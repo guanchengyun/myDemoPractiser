deb0c616fd975abf65131a4e2ddcbc188821f7ab
guanchengyun <guanchengyun@gmail.com>
2014-07-10 18:33:48 +0800

    Initial Commit

---
+++ 数字健身/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  数字健身
+//
+//  Created by 城云 官 on 14-7-10.
+//  Copyright (c) 2014年 yuekang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 数字健身/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  数字健身
+//
+//  Created by 城云 官 on 14-7-10.
+//  Copyright (c) 2014年 yuekang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
